359c82ea2b7e12e2a086257ac7dadc889b4e762f
Yogapriya-02 <k7.yogapriya@gmail.com>
2023-10-11 15:29:25 +0530

    file changes

--- config.js
+++ config.js
@@ -1,41 +1,101 @@
-const xml_path ={
-	Article_Title : `//*[@class-name="ArtTitle"]`,
-	//Abstract_Head : `//*[@class-name = "AbsHead"]`,
-	//Abstract_Title : `//*[@class-name="AbsTitle"]`,
-	//Abstract_Para : `//*[@class-name="AbsPara"]`,
-	//References : `//*[@class-name = "RefText"]`,
-	//Corresponding_Author : [`//contrib/name/prefix`, `//contrib/name/given-names`,`//contrib/name/surname`],
-};
+const xml_paths =[
+	{
+		Article_Title : [`//*[@class-name="ArtTitle"]`]
+	},
+	{
+		Abstract_Head : [`//*[@class-name = "AbsHead"]`]
+	},
+	{
+		Abstract_Title : [`//*[@class-name="AbsTitle"]`]
+	},
+	{
+		Abstract_Para : [`//*[@class-name="AbsPara"]`]
+	},
+	{
+		References : [`//*[@class-name = "RefText"]`]
+	},
+	{
+		Author : [`//contrib/name`,`./prefix` ,`./given-names` ,`./surname`]
+	},
+	{
+		Corres_Author : [`//author-notes/corresp/name`]
+	},
+	{
+		Affiliation :[`//aff`]
+	},
+	{
+		Figure_Caption : [`.//p[@class = "jrnlFigCaption"]`]
+	},
+	{
+		Table_Caption : [`.//p[@class = "jrnlTblCaption"]`]
+	},
+	{
+		Figure_Citation : [`.//span[@class="jrnlFigRef"]`]
+	},
+	{
+		Table_Citation : [`.//span[@class="jrnlTblRef"]`]
+	},
+	/*{
+		//Corres_Affiliation : [`//author-notes/corresp/fn[@fn-type="corresp-address"]`]
+	},*/
+	{
+		Keywords : [`.//kwd-group`]
+	}
+];
 
-const html_path ={
-	Article_Title : `//*[@data-class="jrnlArtTitle"]`,
-	//Abstract_Head : `//*[@data-class="jrnlAbsHead"]`,
-	//Abstract_Title : `//*[@data-class="jrnlAbsPara"]/b`,
-	//Abstract_Para : `//*[@data-class='jrnlAbsPara']/text()`,
-	//References : `//*[@data-class="jrnlRefText"]`,
-	//Corresponding_Author : `//*[@data-class='jrnlAuthors']/text()`,
-};
+const html_paths =[
+	{
+		Article_Title : [`//*[@data-class="jrnlArtTitle"]`],
+		Comparison : true
+	},
+	{
+		Abstract_Head : [`//*[@data-class="jrnlAbsHead"]`],
+		Comparison : true
+	},
+	{
+		Abstract_Title : [`//*[@data-class="jrnlAbsPara"]/b`],
+		Comparison : true
+	},
+	{
+		Abstract_Para : [`//*[@data-class='jrnlAbsPara']/text()`],
+		Comparison : true
+	},
+	{
+		References : [`//*[@data-class="jrnlRefText"]`],
+		Comparison : true
+	},
+	{
+		Author : [`//p[@data-class='jrnlAuthors']`],
+		Comparison : true
+	},
+	{
+		Corres_Author :[`//p[@data-class="jrnlCorresp"]`],
+		Comparison : true
+	},
+	{
+		Affiliation : [`//p[@data-class='jrnlAff']`],
+		Comparison : true
+	},
+	{
+		Figure_Caption : [`.//p[@data-class="jrnlFigCaption"]`],
+		Comparison : true		
+	},
+	{
+		Table_Caption : [`.//p[@data-class="jrnlTblCaption"]`],
+		Comparison : true
+	},
+	{
+		Figure_Citation : [`.//span[@class="jrnlFigRef"]`],
+		Comparison : false
+	},
+	{
+		Table_Citation : [`.//span[@class="jrnlTblRef"]`],
+		Comparison : false
+	},
+	{
+		Keywords : [`.//p[@data-name="keywords"]`],
+		Comparison : true
+	}
+];
 
-
-/*function execution(){
-	let a = Object.keys(xml_path);
-	let b = Object.keys(html_path);
-	if(a.length == b.length){
-    	for(var i=0;i<a.length;i++){
-        	console.log(i+"");
-        	let index = a[i];
-        	if(html_path.hasOwnProperty(index)){
-            	print(xml_path[index]);
-            	//print(html_path[index]);
-        	}
-        }
-  
-    }
-}
-function print(str){
-    console.log(str);
-}
-
-execution();*/
-
-module.exports = {xml_path,html_path};
+module.exports = {xml_paths,html_paths}; 
